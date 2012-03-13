ce280fb56e6ea9200cbbb7ac446468e1b86b6eab
John Swinbank <swinbank@transientskp.org>
2012-03-13 13:37:31 +0100

    initial import

---
+++ run_Calibrate.sh
@@ -0,0 +1,100 @@
+#!/bin/sh
+obs_id=$1
+beam=$2
+band=$3
+key=$4
+skyModel=$5
+calModel=$6
+if [ "${beam}" = 0 ]; then 
+    targ_band=0`echo $band | bc`
+    cal_band=`echo $band+16 | bc`
+else 
+    if [ "${beam}" =  1 ]; then
+        targ_band=`echo $band+8 | bc`
+    cal_band=`echo $band+16 | bc` 
+    
+    fi
+fi
+
+combined=${obs_id}_SAP00${beam}_BAND${band}.MS
+
+echo "Starting run_Calibrate.csh" `date`
+
+#uncomment to move data to your working area
+#echo "Gathering data..." `date`
+#for node in `seq -w 1 100`; do
+#    echo $node
+#    scp -r locus$node:/data/scratch/pipeline/${obs_id}*/*SB${band}*_target_sub* .
+#    scp -r locus$node:/data/scratch/pipeline/${obs_id}*/*SB${cal_band}*_target_sub* .
+#done
+#echo "scp-ing done!" `date`
+
+#for file in *MS.dppp; do
+#    echo rficonsole -indirect-read $file
+#    rficonsole -indirect-read $file
+#done
+#exit
+
+mkdir log
+mkdir plots
+
+for num in `seq 0 9`; do
+    echo "Starting work on subband" $num `date`
+    source=${obs_id}_SAP00${beam}_SB${targ_band}${num}_target_sub.MS.dppp
+    cal=${obs_id}_SAP002_SB${cal_band}${num}_target_sub.MS.dppp
+    
+    rm -rf ${combined}
+    
+    makevds ~pizzo/cep2.clusterdesc ${source}
+    makevds ~pizzo/cep2.clusterdesc ${cal}
+    combinevds ${source}.gds ${source}.vds
+    combinevds ${cal}.gds ${cal}.vds
+    
+    echo "Calibrating CALIBRATOR..."
+    #calibrate -f --key ${key} --cluster-desc ~pizzo/cep2.clusterdesc --db ldb002 --db-user postgres ${cal}.gds /home/csobey/MSSS/cal.parset ${calModel} `pwd` > log/calibrate_cal_${num}.txt
+    calibrate -f --key ${key} --cluster-desc ~pizzo/cep2.clusterdesc --db ldb002 --db-user postgres ${cal}.gds ../cal.parset ${calModel} `pwd` > log/calibrate_cal_${num}.txt
+    
+    echo "Zapping suspect points..."
+    ~swinbank/edit_parmdb/edit_parmdb.py --sigma=2 --auto ${cal}/instrument/
+    
+    echo "Making diagnostic plots..."
+    ~heald/bin/solplot.py -q -m -o SB${cal_band}${num} ${cal}/instrument/ 
+    
+    echo "Calibrating IMAGE..."
+    #calibrate -f --key ${key} --cluster-desc ~pizzo/cep2.clusterdesc --db ldb002 --db-user postgres --instrument-db ${cal}/instrument ${source}.gds /home/csobey/MSSS/correct.parset /home/hassall/MSSS/dummy.model `pwd` > log/calibrate_image_${num}.txt
+    calibrate -f --key ${key} --cluster-desc ~pizzo/cep2.clusterdesc --db ldb002 --db-user postgres --instrument-db ${cal}/instrument ${source}.gds ../correct.parset /home/hassall/MSSS/dummy.model `pwd` > log/calibrate_image_${num}.txt
+    
+    echo "Finished subband" ${num} `date`
+done #end of loop
+
+echo "msin=[${obs_id}_SAP00${beam}_SB${targ_band}"`seq -s "_target_sub.MS.dppp, ${obs_id}_SAP00${beam}_SB${targ_band}" 0 9`"_target_sub.MS.dppp]"> NDPPP.parset
+echo "msin.missingdata=true" >> NDPPP.parset
+echo "msin.orderms=false" >> NDPPP.parset
+echo "msin.datacolumn=CORRECTED_DATA" >> NDPPP.parset
+echo "msout="${combined} >> NDPPP.parset
+echo "steps=[]" >> NDPPP.parset
+echo "" >> NDPPP.parset
+
+echo "Starting work on combined subbands" `date`
+echo "Combining Subbands..."
+NDPPP NDPPP.parset
+
+echo "Making FINAL vds"
+makevds ~pizzo/cep2.clusterdesc ${combined}
+echo "Combining FINAL vds"
+combinevds ${combined}.gds ${combined}.vds
+
+echo "rficonsole..."
+echo "Removing RFI... " `date`
+rficonsole -indirect-read ${combined}
+
+echo "Calibrating FINAL IMAGE"
+echo "Starting phase-only calibration" `date`
+#calibrate -f --key ${key} --cluster-desc ~pizzo/cep2.clusterdesc --db ldb002 --db-user postgres ${combined}.gds /home/csobey/MSSS/phaseonly.parset ${skyModel} `pwd` >log/calibrate_final_image2.txt
+calibrate -f --key ${key} --cluster-desc ~pizzo/cep2.clusterdesc --db ldb002 --db-user postgres ${combined}.gds ../phaseonly.parset ${skyModel} `pwd` >log/calibrate_final_image2.txt
+
+echo "Finished phase-only calibration" `date`
+echo "run_Calibrate.csh Finished" `date`
+mv ${key}* log/
+mv SB*.pdf plots
+
